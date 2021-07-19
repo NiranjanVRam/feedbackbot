c1df9635f4fe8a5a48ff13f122d21f0ad9285b0d
NiranjanVRam <83854040+NiranjanVRam@users.noreply.github.com>
2021-07-19 14:04:34 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -15,7 +15,7 @@ async def cb_handler(c, m):
       button = [[InlineKeyboardButton("cancel", callback_data="cancel")]]
       markup = InlineKeyboardMarkup(button)
       await m.message.delete()
-      await c.send_message(chat_id=m.message.chat.id, text="Send your feed back here I will notify the admin.", reply_markup=markup)
+      await c.send_message(chat_id=m.message.chat.id, text="Send your Feedback here.. I will notify the admin.", reply_markup=markup)
 
   if "cancel" in cb_data:
       if m.from_user.id in Config.feedback:
@@ -43,13 +43,13 @@ async def cb_handler(c, m):
           NS = await feedtext.forward(int(i))
           await NS.reply_text("Send the reply", reply_markup=markup, quote=True)
       await m.message.delete()
-      await c.send_message(chat_id=m.message.chat.id, text="Feedback sent successfully. Hope you will get reply soon")
+      await c.send_message(chat_id=m.message.chat.id, text="Feedback sent successfully. You will get reply as soon as possible!!")
 
 
   if "reply" in cb_data:
       id = m.data.split("+")[1]
       Config.SEND.append(id)
-      await c.send_message(chat_id=m.message.chat.id, text="Reply me the text which you wantedto send him")
+      await c.send_message(chat_id=m.message.chat.id, text="Reply me the text which you wanted to send him!!")
 
   if "about" in cb_data:
       await m.message.delete()
